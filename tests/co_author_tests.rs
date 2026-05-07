d5d85f73b121de7d7d32099eb2aacc1514cd4ff7
Wessel Bindt <wesselbindt@gmail.com>
2026-05-07 22:39:23 +0200

    Get rid of bizarre return values for tests

--- tests/co_author_tests.rs
+++ tests/co_author_tests.rs
@@ -1,5 +1,3 @@
-use anyhow::Result;
-
 use crate::common::test_app::TestApp;
 use crate::common::use_git::set_up_basic_repo;
 use crate::common::write_file::write_file;
@@ -38,30 +36,28 @@ fn test_commit_missing_co_author_fails() {
 }
 
 #[test]
-fn test_commit_with_alias() -> Result<()> {
+fn test_commit_with_alias() {
     let app = TestApp::new();
     let repo = set_up_basic_repo(app.base_dir.path());
     let repo_path = repo.as_path();
 
-    app.set_aliases("jdoe:John Doe <jdoe@example.com>\n")?;
+    app.set_aliases("jdoe:John Doe <jdoe@example.com>\n").expect("should succeed");
 
     write_file(repo_path, "alias.txt", "alias");
-    app.commit(repo_path, "alias commit", Some("@jdoe"))?;
+    app.commit(repo_path, "alias commit", Some("@jdoe")).expect("should succeed");
 
     let log = g_cli::cmd_log(repo_path, true).expect("g l");
     assert!(log.contains("alias commit"));
     assert!(log.contains("Co-authored-by: John Doe <jdoe@example.com>"));
-
-    Ok(())
 }
 
 #[test]
-fn test_commit_with_unknown_alias_fails() -> Result<()> {
+fn test_commit_with_unknown_alias_fails() {
     let app = TestApp::new();
     let repo = set_up_basic_repo(app.base_dir.path());
     let repo_path = repo.as_path();
 
-    app.set_aliases("known:Name <email@example.com>")?;
+    app.set_aliases("known:Name <email@example.com>").expect("should succeed");
 
     write_file(repo_path, "unknown.txt", "unknown");
     let result = app.commit(repo_path, "unknown alias", Some("@unknown"));
@@ -72,6 +68,4 @@ fn test_commit_with_unknown_alias_fails() -> Result<()> {
         .contains("Unknown co-author alias: @unknown"));
     assert!(err.to_string().contains("Please add it to"));
     assert!(err.to_string().contains("known:Name <email@example.com>"));
-
-    Ok(())
 }
