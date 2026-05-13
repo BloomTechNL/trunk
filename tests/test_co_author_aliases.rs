38e48680efd4b72493648565814979d2b652ef38
Wessel Bindt <wesselbindt@gmail.com>
2026-05-13 12:53:10 +0200

    Add multiple case
    (Solo-work)

--- tests/test_co_author_aliases.rs
+++ tests/test_co_author_aliases.rs
@@ -51,11 +51,46 @@ fn test_unknown_alias() {
     let co_author_aliases_path = base_dir.path().join("aliases");
     let co_author_aliases = RealCoAuthorAliases::new(co_author_aliases_path.clone());
 
-    let formatted_alias = co_author_aliases
-        .format_alias("@harry");
+    let formatted_alias = co_author_aliases.format_alias("@harry");
+
+    assert_eq!(formatted_alias, None,);
+}
+
+#[test]
+fn test_multiple_aliases() {
+    let base_dir = TempDir::new().unwrap();
+    let co_author_aliases_path = base_dir.path().join("aliases");
+    let co_author_aliases = RealCoAuthorAliases::new(co_author_aliases_path.clone());
+
+    add_alias(
+        &co_author_aliases_path,
+        "@harry",
+        "Harry Bronchitis",
+        "harry.bronchitis@example.com",
+    )
+    .expect("Should succeed");
+    add_alias(
+        &co_author_aliases_path,
+        "@sally",
+        "Sally Cholera",
+        "sally.cholera@example.com",
+    )
+    .expect("Should succeed");
+
+    let formatted_alias_1 = co_author_aliases
+        .format_alias("@harry")
+        .expect("Should succeed");
+    let formatted_alias_2 = co_author_aliases
+        .format_alias("@sally")
+        .expect("Should succeed");
 
     assert_eq!(
-        formatted_alias,
-        None,
+        formatted_alias_1,
+        "Harry Bronchitis <harry.bronchitis@example.com>"
+    );
+
+    assert_eq!(
+        formatted_alias_2,
+        "Sally Cholera <sally.cholera@example.com>"
     );
 }
